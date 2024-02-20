8d5964d079993e2691af1bf59893072f33749726
Kaelan Smith <kaelancsmith@gmail.com>
2024-02-20 14:08:46 -0800

    new build

--- dist/components/TypographyListItem.js
+++ dist/components/TypographyListItem.js
@@ -1,6 +1,6 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
 import { cx } from "@cloakui/styles";
 import { DynamicHtmlParser } from "./DynamicHtmlParser";
 export const TypographyListItem = ({ content, className, style, children, ...props }) => {
-    return (_jsxs(_Fragment, { children: [_jsx("li", { className: cx("list-circle leading-7 pl-2", className), style: style, ...props, children: _jsx(DynamicHtmlParser, { children: content }) }), _jsx(DynamicHtmlParser, { children: children })] }));
+    return (_jsx(_Fragment, { children: _jsxs("li", { className: cx("list-circle leading-7 pl-2", className), style: style, ...props, children: [_jsx(DynamicHtmlParser, { children: content }), _jsx(DynamicHtmlParser, { children: children })] }) }));
 };

d9ada27aef47f7045cbd34d74d10e4f66a199f03
Kaelan Smith <kaelancsmith@gmail.com>
2024-02-20 14:07:51 -0800

    update Container component

--- dist/components/Container.js
+++ dist/components/Container.js
@@ -1,8 +1,7 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import { cx } from "@cloakui/styles";
-export function Container({ className, innerClassName, children, as = "div", ...props }) {
+export function Container({ className = "", cntrClassName = "", children, as = "div", ...props }) {
     const Element = as;
-    const defaultInnerClassNames = "px-4 sm:px-6 lg:px-9 mx-auto max-w-7xl lg:max-w-8xl";
     const hasBgColor = className?.includes("bg-") || false;
-    return (_jsx(Element, { className: cx(!hasBgColor && defaultInnerClassNames, !hasBgColor && innerClassName, className), ...props, children: hasBgColor ? (_jsx("div", { className: cx(defaultInnerClassNames, innerClassName), children: children })) : (children) }));
+    return (_jsx(Element, { className: cx(!hasBgColor && cntrClassName, className), ...props, children: hasBgColor ? _jsx("div", { className: cntrClassName, children: children }) : children }));
 }

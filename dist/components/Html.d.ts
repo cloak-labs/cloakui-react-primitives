062a6e4b64edf7117ed6ae8f336265d97e2efb71
Kaelan Smith <kaelancsmith@gmail.com>
2024-09-19 12:28:41 -0700

    delete Html component

--- dist/components/Html.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function Html({ content, className, style, ...props }: {
-    [x: string]: any;
-    content: any;
-    className: any;
-    style: any;
-}): import("react/jsx-runtime").JSX.Element;
--- dist/components/Html.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { DynamicHtmlParser } from "./DynamicHtmlParser";
-export function Html({ content, className, style, ...props }) {
-    return (_jsx("div", { className: className, style: style, ...props, children: _jsx(DynamicHtmlParser, { children: content }) }));
-}

4ca810a538644acd17b956082c4226a47b396e63
Kaelan Smith <kaelancsmith@gmail.com>
2024-09-25 11:47:46 -0700

    use deepMerge from @kaelan/deep-merge-ts, fix withProps TS stuff

--- dist/components/withProps.d.ts
+++ dist/components/withProps.d.ts
@@ -1,3 +1,3 @@
 import React from "react";
-import type { Component, DefaultProps, PropsObject } from "../types";
-export declare function withProps<T extends Component>(Component: T, defaultProps: DefaultProps<T>): React.ForwardRefExoticComponent<React.PropsWithoutRef<PropsObject<T>> & React.RefAttributes<React.ElementRef<T>>>;
+import type { Component, DefaultProps } from "../types";
+export declare function withProps<T extends Component>(Component: T, defaultProps: DefaultProps<T>): React.ForwardRefExoticComponent<React.PropsWithoutRef<React.ComponentProps<T>> & React.RefAttributes<React.ElementRef<T>>>;
--- src/components/withProps.tsx
+++ src/components/withProps.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { cx } from "@cloakui/styles";
-import { deepMerge } from "cloakcms";
-import type { Component, DefaultProps, PropsObject } from "../types";
+import { deepMerge } from "@kaelan/deep-merge-ts";
+import type { Component, DefaultProps, DefaultPropsFunction } from "../types";
 
 export function withProps<T extends Component>(
   Component: T,
   defaultProps: DefaultProps<T>
 ) {
-  const ComponentWithClassName = Component as React.FC<{ className: string }>;
+  const ComponentWithClassName = Component as React.ForwardRefExoticComponent<
+    React.ComponentPropsWithoutRef<T> & React.RefAttributes<React.ElementRef<T>>
+  >;
 
-  return React.forwardRef<React.ElementRef<T>, PropsObject<T>>(
+  return React.forwardRef<React.ElementRef<T>, React.ComponentProps<T>>(
     function ExtendComponent(props, ref) {
-      const isDefaultPropsFunction = typeof defaultProps == "function";
+      const isDefaultPropsFunction = typeof defaultProps === "function";
       const finalDefaultProps = isDefaultPropsFunction
-        ? (defaultProps as Function)(props)
+        ? (defaultProps as DefaultPropsFunction<T>)(props)
         : defaultProps;
 
       // if the default props is a function, it means that default props should override instance-level props:
-      const finalProps = isDefaultPropsFunction
-        ? deepMerge(props, finalDefaultProps)
-        : deepMerge(finalDefaultProps, props);
+      const finalProps = (
+        isDefaultPropsFunction
+          ? deepMerge(props, finalDefaultProps)
+          : deepMerge(finalDefaultProps, props)
+      ) as React.ComponentPropsWithoutRef<T>;
 
       return (
         <ComponentWithClassName
